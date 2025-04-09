27e583db4a90f8aa3c19d79b47f5c52958564890
Pushpaju018 <ajaypanwr018@gmail.com>
2025-04-09 11:25:27 +0530

    Update flood-alert-system.ino

--- Flood-Alert-System/flood-alert-system.ino
+++ Flood-Alert-System/flood-alert-system.ino
@@ -21,7 +21,7 @@ SoftwareSerial fonaSS = SoftwareSerial(FONA_TX, FONA_RX);
 Adafruit_FONA fona = Adafruit_FONA(FONA_RST);
 
 
-char PHONE_1[21] = ""; // Enter your Mobile Number 
+char PHONE_1[21] = "+919897538995"; // Enter your Mobile Number 
 
 char tempalert[141]= "Flood Alert"  ; 
 void setup()
